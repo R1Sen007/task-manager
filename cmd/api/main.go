b3a79d87dc6eb4372ed23af61d423d0206253a90
Pavel <ptyrtin@honeycorn.ru>
2026-07-30 00:44:08 +0700

    feat: update service pack and started main package

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+
+	"github.com/R1Sen007/task-manager/internal/task"
+)
+
+type CreateTaskInput struct {
+	Title       string
+	Description string
+}
+
+func main() {
+	repo := task.NewRepository()
+	service := task.NewService(repo)
+
+	inputs := []CreateTaskInput{
+		{Title: "Learn Go", Description: "Understand errors"},
+		{Title: "", Description: "This should fail"},
+		{Title: "Learn interfaces", Description: "Go deeper"},
+	}
+
+	for _, task_input := range inputs {
+		tsk, err := service.CreateTask(task_input.Title, task_input.Description)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(tsk.Title, tsk.Description)
+	}
+}
--- internal/task/service.go
+++ internal/task/service.go
@@ -1,5 +1,7 @@
 package task
 
+import "errors"
+
 type Service struct {
 	repo *Repository
 }
@@ -8,4 +10,26 @@ func NewService(repo *Repository) *Service {
 	return &Service{
 		repo: repo,
 	}
+}
+
+func (s *Service) CreateTask(title, description string) (Task, error) {
+	if title == "" {
+		return Task{}, errors.New("title can't be empty")
+	}
+
+	return s.repo.Create(
+		Task{
+			Title:       title,
+			Description: description,
+			Done:        false,
+		},
+	), nil
+}
+
+func (s *Service) GetTask(id int64) (Task, bool) {
+	return s.repo.GetByID(id)
+}
+
+func (s *Service) DeleteTask(id int64) bool {
+	return s.repo.Delete(id)
 }
