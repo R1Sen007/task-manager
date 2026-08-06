3ff5b701d279eaef4fcf744e73717c046985b782
Pavel <ptyrtin@honeycorn.ru>
2026-08-06 23:40:40 +0700

    test: add success create service test

--- internal/task/service_test.go
+++ internal/task/service_test.go
@@ -7,10 +7,13 @@ import (
 
 type fakeRepository struct {
 	createCalled bool
+	createdTask  Task
 }
 
 func (r *fakeRepository) Create(task Task) Task {
 	r.createCalled = true
+	r.createdTask = task
+
 	task.ID = 1
 	return task
 }
@@ -61,4 +64,42 @@ func TestService_CreateTask_EmptyTitle(t *testing.T) {
 		t.Fatal("repository Create must not be called")
 	}
 
+}
+
+func TestService_CreateTask_Success(t *testing.T) {
+	fakeRepo := &fakeRepository{}
+	service := NewService(fakeRepo)
+
+	createdTask, err := service.CreateTask("smTitle", "descr")
+	if err != nil {
+		t.Fatalf("expected no error, got %v", err)
+	}
+
+	if !fakeRepo.createCalled {
+		t.Fatal("expected repository Create to be called")
+	}
+
+	if fakeRepo.createdTask.Title != "smTitle" {
+		t.Fatalf(
+			"expected repository task title %q, got %q",
+			"smTitle",
+			fakeRepo.createdTask.Title,
+		)
+	}
+
+	if fakeRepo.createdTask.Description != "descr" {
+		t.Fatalf(
+			"expected repository task description %q, got %q",
+			"descr",
+			fakeRepo.createdTask.Description,
+		)
+	}
+
+	if fakeRepo.createdTask.Done {
+		t.Fatal("expected repository task Done to be false")
+	}
+
+	if createdTask.ID != 1 {
+		t.Fatalf("expected created task ID 1, got %d", createdTask.ID)
+	}
 }
